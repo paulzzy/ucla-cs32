47353039e0deafd6ddde6a3b4e891e1f23214ca0
Paul Zhang <paulz@ucla.edu>
2022-04-10 16:57:25 -0700

    Problem 1: Declare member functions as `const` when appropriate

--- homework1/Sequence.h
+++ homework1/Sequence.h
@@ -1,54 +1,53 @@
-class Sequence
-{
-  public:
-    Sequence();    // Create an empty sequence (i.e., one whose size() is 0).
-    bool empty();  // Return true if the sequence is empty, otherwise false.
-    int size();    // Return the number of items in the sequence.
-    int insert(int pos, const std::string& value);
-      // Insert value into the sequence so that it becomes the item at
-      // position pos.  The original item at position pos and those that
-      // follow it end up at positions one greater than they were at before.
-      // Return pos if 0 <= pos <= size() and the value could be
-      // inserted.  (It might not be, if the sequence has a fixed capacity,
-      // e.g., because it's implemented using a fixed-size array.)  Otherwise,
-      // leave the sequence unchanged and return -1.  Notice that
-      // if pos is equal to size(), the value is inserted at the end.
+class Sequence {
+public:
+  Sequence();         // Create an empty sequence (i.e., one whose size() is 0).
+  bool empty() const; // Return true if the sequence is empty, otherwise false.
+  int size() const;   // Return the number of items in the sequence.
+  int insert(int pos, const std::string &value);
+  // Insert value into the sequence so that it becomes the item at
+  // position pos.  The original item at position pos and those that
+  // follow it end up at positions one greater than they were at before.
+  // Return pos if 0 <= pos <= size() and the value could be
+  // inserted.  (It might not be, if the sequence has a fixed capacity,
+  // e.g., because it's implemented using a fixed-size array.)  Otherwise,
+  // leave the sequence unchanged and return -1.  Notice that
+  // if pos is equal to size(), the value is inserted at the end.
 
-    int insert(const std::string& value);
-      // Let p be the smallest integer such that value <= the item at
-      // position p in the sequence; if no such item exists (i.e.,
-      // value > all items in the sequence), let p be size().  Insert
-      // value into the sequence so that it becomes the item in position
-      // p.  The original item at position p and those that follow it end
-      // up at positions one greater than before.  Return p if the value
-      // was actually inserted.  Return -1 if the value was not inserted
-      // (perhaps because the sequence has a fixed capacity and is full).
-     
-    bool erase(int pos);
-      // If 0 <= pos < size(), remove the item at position pos from
-      // the sequence (so that all items that followed that item end up at
-      // positions one lower than they were at before), and return true.
-      // Otherwise, leave the sequence unchanged and return false.
-     
-    int remove(const std::string& value);
-      // Erase all items from the sequence that == value.  Return the
-      // number of items removed (which will be 0 if no item == value).
+  int insert(const std::string &value);
+  // Let p be the smallest integer such that value <= the item at
+  // position p in the sequence; if no such item exists (i.e.,
+  // value > all items in the sequence), let p be size().  Insert
+  // value into the sequence so that it becomes the item in position
+  // p.  The original item at position p and those that follow it end
+  // up at positions one greater than before.  Return p if the value
+  // was actually inserted.  Return -1 if the value was not inserted
+  // (perhaps because the sequence has a fixed capacity and is full).
 
-    bool get(int pos, std::string& value);
-      // If 0 <= pos < size(), copy into value the item at position pos
-      // of the sequence and return true.  Otherwise, leave value unchanged
-      // and return false.
+  bool erase(int pos);
+  // If 0 <= pos < size(), remove the item at position pos from
+  // the sequence (so that all items that followed that item end up at
+  // positions one lower than they were at before), and return true.
+  // Otherwise, leave the sequence unchanged and return false.
 
-    bool set(int pos, const std::string& value);
-      // If 0 <= pos < size(), replace the item at position pos in the
-      // sequence with value and return true.  Otherwise, leave the sequence
-      // unchanged and return false.
+  int remove(const std::string &value);
+  // Erase all items from the sequence that == value.  Return the
+  // number of items removed (which will be 0 if no item == value).
 
-    int find(const std::string& value);
-      // Let p be the smallest integer such that value == the item at
-      // position p in the sequence; if no such item exists, let p be -1.
-      // Return p.
+  bool get(int pos, std::string &value) const;
+  // If 0 <= pos < size(), copy into value the item at position pos
+  // of the sequence and return true.  Otherwise, leave value unchanged
+  // and return false.
 
-    void swap(Sequence& other);
-      // Exchange the contents of this sequence with the other one.
+  bool set(int pos, const std::string &value);
+  // If 0 <= pos < size(), replace the item at position pos in the
+  // sequence with value and return true.  Otherwise, leave the sequence
+  // unchanged and return false.
+
+  int find(const std::string &value) const;
+  // Let p be the smallest integer such that value == the item at
+  // position p in the sequence; if no such item exists, let p be -1.
+  // Return p.
+
+  void swap(Sequence &other);
+  // Exchange the contents of this sequence with the other one.
 };
