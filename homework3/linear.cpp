570d7314f7bf93c4974d94c9cd0fa536823150fd
Paul Zhang <paulz@ucla.edu>
2022-05-03 15:33:12 -0700

    Fix behavior when no elements satisfy the condition

--- homework3/linear.cpp
+++ homework3/linear.cpp
@@ -44,14 +44,22 @@ int countFalse(const std::string a[], int n) {
 // element, return -1.
 // NOLINTNEXTLINE(misc-no-recursion)
 int firstFalse(const std::string a[], int n) {
+  // Negative size
   if (n <= 0) {
     return -1;
   }
 
+  // First element that satisfies the condition
   if (!somePredicate(a[0])) {
     return 0;
   }
 
+  // No elements satisfy the condition
+  if (firstFalse(a + 1, n - 1) == -1) {
+    return -1;
+  }
+
+  // Increment the index when checking the next element
   return 1 + firstFalse(a + 1, n - 1);
 }
 
