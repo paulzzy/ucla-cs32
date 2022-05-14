d006b63ed0adbb3387818ddb7a6099c6660e3ab1
Paul Zhang <paulz@ucla.edu>
2022-05-14 15:15:03 -0700

    Add test cases for `HumanPlayer` and `MediocrePlayer`

--- project3/main.cpp
+++ project3/main.cpp
@@ -172,4 +172,132 @@ void board_tests() {
 
   std::cerr << "Passed `Board` test cases 🤩" << std::endl;
 }
+
+void human_player_tests() {
+  Game g{MAXROWS, MAXCOLS};
+  add_test_ships(g);
+  Player *h = createPlayer("human", "human_test", g);
+
+  Board b{g};
+
+  assert(h->isHuman());
+  assert(h->placeShips(b));
+
+  std::cerr << "Enter 0 0 when prompted for the attack point." << std::endl;
+  const Point p1 = h->recommendAttack();
+  assert(p1.r == 0 && p1.c == 0);
+
+  std::cerr << std::endl;
+  std::cerr << "Enter " << MAXROWS << " " << MAXCOLS
+            << " when prompted for the attack point.\n";
+  const Point p2 = h->recommendAttack();
+  assert(p2.r == MAXROWS && p2.c == MAXCOLS);
+}
+
+void mediocre_player_tests() {
+  Game g{MAXROWS, MAXCOLS};
+  addStandardShips(g);
+
+  Player *m = createPlayer("mediocre", "test_mediocre", g);
+
+  Board b{g};
+
+  assert(!m->isHuman());
+
+// #define MEDIOCRE_TEST_PLACING_SHIPS
+#ifdef MEDIOCRE_TEST_PLACING_SHIPS
+
+  const int MAX_PLACES = 100;
+  int fail_count = 0;
+
+  for (int i = 0; i < MAX_PLACES; i++) {
+    if (m->placeShips(b)) {
+      b.display(false);
+      std::cerr << "Above is Board " << i
+                << ": Should contain the 5 standard Battleship vessels, "
+                   "arranged randomly.\n\n";
+      b.clear();
+    } else {
+      fail_count++;
+      std::cerr << "Above is Board " << i
+                << ": Could not place all ships.\n\n ";
+    }
+  }
+
+  std::cerr << "Could not place " << fail_count << " ships out of "
+            << MAX_PLACES << " total.\n";
+
+#endif
+
+  std::cerr << "Passed mediocre player test cases." << std::endl;
+}
+
+int main() {
+// #define PLAY_DEFAULT_GAMES
+#ifdef PLAY_DEFAULT_GAMES
+
+  const int NTRIALS = 10;
+
+  cout << "Select one of these choices for an example of the game:" << endl;
+  cout << "  1.  A mini-game between two mediocre players" << endl;
+  cout << "  2.  A mediocre player against a human player" << endl;
+  cout << "  3.  A " << NTRIALS
+       << "-game match between a mediocre and an awful player, with no pauses"
+       << endl;
+  cout << "Enter your choice: ";
+  string line;
+  getline(cin, line);
+  if (line.empty()) {
+    cout << "You did not enter a choice" << endl;
+  } else if (line[0] == '1') {
+    Game g(2, 3);
+    g.addShip(2, 'R', "rowboat");
+    Player *p1 = createPlayer("mediocre", "Popeye", g);
+    Player *p2 = createPlayer("mediocre", "Bluto", g);
+    cout << "This mini-game has one ship, a 2-segment rowboat." << endl;
+    g.play(p1, p2);
+    delete p1;
+    delete p2;
+  } else if (line[0] == '2') {
+    Game g(10, 10);
+    addStandardShips(g);
+    Player *p1 = createPlayer("mediocre", "Mediocre Midori", g);
+    Player *p2 = createPlayer("human", "Shuman the Human", g);
+    g.play(p1, p2);
+    delete p1;
+    delete p2;
+  } else if (line[0] == '3') {
+    int nMediocreWins = 0;
+
+    for (int k = 1; k <= NTRIALS; k++) {
+      cout << "============================= Game " << k
+           << " =============================" << endl;
+      Game g(10, 10);
+      addStandardShips(g);
+      Player *p1 = createPlayer("awful", "Awful Audrey", g);
+      Player *p2 = createPlayer("mediocre", "Mediocre Mimi", g);
+      Player *winner =
+          (k % 2 == 1 ? g.play(p1, p2, false) : g.play(p2, p1, false));
+      if (winner == p2) {
+        nMediocreWins++;
+      }
+      delete p1;
+      delete p2;
+    }
+    cout << "The mediocre player won " << nMediocreWins << " out of " << NTRIALS
+         << " games." << endl;
+    // We'd expect a mediocre player to win most of the games against
+    // an awful player.  Similarly, a good player should outperform
+    // a mediocre player.
+  } else {
+    cout << "That's not one of the choices." << endl;
+  }
+
+#endif
+
+  // Custom tests
+  // game_tests();
+  // board_tests();
+  // human_player_tests();
+  mediocre_player_tests();
 }
