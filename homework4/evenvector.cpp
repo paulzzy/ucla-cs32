0abcdb38372357bcdcf4be30023a2135b415a23c
Paul Zhang <paulz@ucla.edu>
2022-05-19 15:21:05 -0700

    Format and apply `clang-tidy` linter fixes

--- homework4/evenvector.cpp
+++ homework4/evenvector.cpp
@@ -1,31 +1,28 @@
-#include <vector>
 #include <algorithm>
-#include <iostream>
 #include <cassert>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-  // Remove the even integers from v.
-  // It is acceptable if the order of the remaining even integers is not
-  // the same as in the original vector.
-void removeEven(vector<int>& v)
-{
+// Remove the even integers from v.
+// It is acceptable if the order of the remaining even integers is not
+// the same as in the original vector.
+void removeEven(vector<int> &v) {}
+
+void test() {
+  int a[8] = {1, 7, 6, 5, 8, 4, 3, 2};
+  vector<int> x(a, a + 8); // construct x from the array
+  assert(x.size() == 8 && x.front() == 1 && x.back() == 2);
+  removeEven(x);
+  assert(x.size() == 4);
+  sort(x.begin(), x.end());
+  int expect[4] = {1, 3, 5, 7};
+  for (int k = 0; k < 4; k++) {
+    assert(x[k] == expect[k]);
+  }
 }
 
-void test()
-{
-    int a[8] = { 1, 7, 6, 5, 8, 4, 3, 2 };
-    vector<int> x(a, a+8);  // construct x from the array
-    assert(x.size() == 8 && x.front() == 1 && x.back() == 2);
-    removeEven(x);
-    assert(x.size() == 4);
-    sort(x.begin(), x.end());
-    int expect[4] = { 1, 3, 5, 7 };
-    for (int k = 0; k < 4; k++)
-        assert(x[k] == expect[k]);
-}
-
-int main()
-{
-    test();
-    cout << "Passed" << endl;
+int main() {
+  test();
+  cout << "Passed" << endl;
 }
--- homework4/sorts.cpp
+++ homework4/sorts.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
 #include <algorithm>
-#include <numeric>  // for std::accumulate
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <numeric> // for std::accumulate
+#include <random>
+#include <string>
+#include <unordered_set>
 #include <utility>
 #include <vector>
-#include <unordered_set>
-#include <string>
-#include <random>
-#include <cmath>
-#include <cassert>
 
 using namespace std;
 
@@ -28,25 +28,18 @@ const bool TEST_INSERTION_SORT = true;
 
 #include <chrono>
 
-class Timer
-{
-  public:
-    Timer()
-    {
-        start();
-    }
-    void start()
-    {
-        m_time = std::chrono::high_resolution_clock::now();
-    }
-    double elapsed() const
-    {   
-        std::chrono::duration<double, std::milli> diff =
-                          std::chrono::high_resolution_clock::now() - m_time;
-        return diff.count();
-    }
-  private:
-    std::chrono::high_resolution_clock::time_point m_time;
+class Timer {
+public:
+  Timer() { start(); }
+  void start() { m_time = std::chrono::high_resolution_clock::now(); }
+  double elapsed() const {
+    std::chrono::duration<double, std::milli> diff =
+        std::chrono::high_resolution_clock::now() - m_time;
+    return diff.count();
+  }
+
+private:
+  std::chrono::high_resolution_clock::time_point m_time;
 };
 
 //========================================================================
@@ -65,228 +58,230 @@ std::default_random_engine generator(aRandomDevice());
 
 using IdType = int;
 
-struct Sensor
-{
-    IdType id;
-    double avg;
-    static const int NREADINGS = 200;
-    double readings[NREADINGS];
-    Sensor(IdType i) : id(i)
-    {
-        std::uniform_int_distribution<> distro(0, 99);
+struct Sensor {
+  IdType id;
+  double avg;
+  static const int NREADINGS = 200;
+  double readings[NREADINGS];
+  Sensor(IdType i) : id(i) {
+    std::uniform_int_distribution<> distro(0, 99);
 
-          // create random sensor readings (from 0 to 99)
-        for (size_t k = 0; k < NREADINGS; k++)
-            readings[k] = distro(generator);
-          // (accumulate computes 0.0 + readings[0] + readings[1] + ...)
-        avg = accumulate(readings, readings + NREADINGS, 0.0) / NREADINGS;
+    // create random sensor readings (from 0 to 99)
+    for (size_t k = 0; k < NREADINGS; k++) {
+      readings[k] = distro(generator);
     }
+    // (accumulate computes 0.0 + readings[0] + readings[1] + ...)
+    avg = accumulate(readings, readings + NREADINGS, 0.0) / NREADINGS;
+  }
 };
 
-inline
-bool compareSensor(const Sensor& lhs, const Sensor& rhs)
-{
-      // The Sensor with the higher average should come first.  If they have
-      // the same average, then the Sensor with the smaller id number should
-      // come first.  Return true iff lhs should come first.  Notice that
-      // this means that a false return means EITHER that rhs should come
-      // first, or there's a tie, so we don't care which comes first,
+inline bool compareSensor(const Sensor &lhs, const Sensor &rhs) {
+  // The Sensor with the higher average should come first.  If they have
+  // the same average, then the Sensor with the smaller id number should
+  // come first.  Return true iff lhs should come first.  Notice that
+  // this means that a false return means EITHER that rhs should come
+  // first, or there's a tie, so we don't care which comes first,
 
-    if (lhs.avg > rhs.avg)
-        return true;
-    if (lhs.avg < rhs.avg)
-        return false;
-    return lhs.id < rhs.id;
-}
-
-inline
-bool compareSensorPtr(const Sensor* lhs, const Sensor* rhs)
-{
-    // TODO: You implement this.  Using the same criteria as compareSensor,
-    //       compare two Sensors POINTED TO by lhs and rhs.  Think about
-    //       how you can do it in one line by calling compareSensor.
-
-    // Just so this will compile for now, we'll pretend every comparison
-    // results in a tie, so there's no preferred ordering.
-    return false;  // Delete this line and write your code instead
-}
-
-void insertion_sort(vector<Sensor>& s, bool comp(const Sensor&, const Sensor&))
-{
-    // TODO: Using the insertion sort algorithm (pp. 332-333 in the
-    //       Carrano book; also <a href="bookcode.html">here</a>), sort s
-    //       according to the ordering relationship passed in as the
-    //       parameter comp.
-
-    // Note:  The insertion sort algorithm on pp. 312-313 of the Carrano
-    // book 6th edition is incorrect; someone made a change from the 5th
-    // edition and messed it up.  See the errata page entry for page 313 at
-    // http://homepage.cs.uri.edu/~carrano/WMcpp6e
-
-    // Just to show you how to use the second parameter, we'll write code
-    // that sorts only a vector of 2 elements.  (This is *not* the
-    // insertion sort algorithm.)
-
-    // Note that if comp(x,y) is true, it means x must end up before y in the
-    // final ordering.
-    if (s.size() == 2  &&  comp(s[1], s[0]))
-        swap(s[0], s[1]);
-}
-
-  // Report the results of a timing test
-
-void report(string caption, double t, const vector<Sensor>& s)
-{
-    if (t >= 1)
-        t = round(100*t) / 100;
-    cout << t << " milliseconds; " << caption
-             << "; first few sensors are\n\t";
-    size_t n = s.size();
-    if (n > 4)
-        n = 4;
-    for (size_t k = 0; k < n; k++)
-        cout << " (" << s[k].id << ", " << s[k].avg << ")";
-    cout << endl;
-}
-
-  // Determine if two vectors have the same elements (in a possibly different
-  // order).
-
-bool is_permutation(const vector<Sensor>& v1, const vector<Sensor>& v2)
-{
-    if (v1.size() != v2.size())
-        return false;
-    unordered_multiset<double> m;
-    m.reserve(v1.size());
-    for (const auto& s : v1)
-        m.insert(s.avg);
-    for (const auto& s : v2)
-    {
-        auto p = m.find(s.avg);
-        if (p == m.end())
-            return false;
-        m.erase(p);
-    }
+  if (lhs.avg > rhs.avg) {
     return true;
+  }
+  if (lhs.avg < rhs.avg) {
+    return false;
+  }
+  return lhs.id < rhs.id;
 }
 
-void sortUsingPtrs(vector<Sensor>& sensors, bool comp(const Sensor*, const Sensor*));
+inline bool compareSensorPtr(const Sensor * /*lhs*/, const Sensor * /*rhs*/) {
+  // TODO: You implement this.  Using the same criteria as compareSensor,
+  //       compare two Sensors POINTED TO by lhs and rhs.  Think about
+  //       how you can do it in one line by calling compareSensor.
 
-int main()
-{
-    size_t nsensors;
-    cout << "Enter number of sensors to sort: ";
-    cin >> nsensors;
-    if ( !cin  ||  nsensors <= 0)
-    {
-        cout << "You must enter a positive number.  Goodbye." << endl;
-        return 1;
+  // Just so this will compile for now, we'll pretend every comparison
+  // results in a tie, so there's no preferred ordering.
+  return false; // Delete this line and write your code instead
+}
+
+void insertion_sort(vector<Sensor> &s,
+                    bool comp(const Sensor &, const Sensor &)) {
+  // TODO: Using the insertion sort algorithm (pp. 332-333 in the
+  //       Carrano book; also <a href="bookcode.html">here</a>), sort s
+  //       according to the ordering relationship passed in as the
+  //       parameter comp.
+
+  // Note:  The insertion sort algorithm on pp. 312-313 of the Carrano
+  // book 6th edition is incorrect; someone made a change from the 5th
+  // edition and messed it up.  See the errata page entry for page 313 at
+  // http://homepage.cs.uri.edu/~carrano/WMcpp6e
+
+  // Just to show you how to use the second parameter, we'll write code
+  // that sorts only a vector of 2 elements.  (This is *not* the
+  // insertion sort algorithm.)
+
+  // Note that if comp(x,y) is true, it means x must end up before y in the
+  // final ordering.
+  if (s.size() == 2 && comp(s[1], s[0])) {
+    swap(s[0], s[1]);
+  }
+}
+
+// Report the results of a timing test
+
+void report(const string &caption, double t, const vector<Sensor> &s) {
+  if (t >= 1) {
+    t = round(100 * t) / 100;
+  }
+  cout << t << " milliseconds; " << caption << "; first few sensors are\n\t";
+  size_t n = s.size();
+  if (n > 4) {
+    n = 4;
+  }
+  for (size_t k = 0; k < n; k++) {
+    cout << " (" << s[k].id << ", " << s[k].avg << ")";
+  }
+  cout << endl;
+}
+
+// Determine if two vectors have the same elements (in a possibly different
+// order).
+
+bool is_permutation(const vector<Sensor> &v1, const vector<Sensor> &v2) {
+  if (v1.size() != v2.size()) {
+    return false;
+  }
+  unordered_multiset<double> m;
+  m.reserve(v1.size());
+  for (const auto &s : v1) {
+    m.insert(s.avg);
+  }
+  for (const auto &s : v2) {
+    auto p = m.find(s.avg);
+    if (p == m.end()) {
+      return false;
     }
+    m.erase(p);
+  }
+  return true;
+}
 
-      // Create a random ordering of id numbers 0 through nsensors-1
-    vector<IdType> ids;
-    for (size_t j = 0; j < nsensors; j++)
-        ids.push_back(IdType(j));
-    shuffle(ids.begin(), ids.end(), generator);  // from <algorithm>
+void sortUsingPtrs(vector<Sensor> &sensors,
+                   bool comp(const Sensor *, const Sensor *));
 
-      // Create a bunch of Sensors
-    vector<Sensor> unorderedSensors;
-    for (size_t k = 0; k < ids.size(); k++)
-        unorderedSensors.push_back(Sensor(ids[k]));
+int main() {
+  size_t nsensors;
+  cout << "Enter number of sensors to sort: ";
+  cin >> nsensors;
+  if (!cin || nsensors <= 0) {
+    cout << "You must enter a positive number.  Goodbye." << endl;
+    return 1;
+  }
 
-      // Create a timer
+  // Create a random ordering of id numbers 0 through nsensors-1
+  vector<IdType> ids;
+  for (size_t j = 0; j < nsensors; j++) {
+    ids.push_back(IdType(j));
+  }
+  shuffle(ids.begin(), ids.end(), generator); // from <algorithm>
 
-    Timer timer;
+  // Create a bunch of Sensors
+  vector<Sensor> unorderedSensors;
+  for (size_t k = 0; k < ids.size(); k++) {
+    unorderedSensors.push_back(Sensor(ids[k]));
+  }
 
-      // Sort the Sensors using the STL sort algorithm.  It uses a variant
-      // of quicksort called introsort.
+  // Create a timer
 
-    vector<Sensor> sensors(unorderedSensors);
-    timer.start();
-    sort(sensors.begin(), sensors.end(), compareSensor);
-    report("STL sort", timer.elapsed(), sensors);
-    assert(is_sorted(sensors.begin(), sensors.end(), compareSensor));  // from <algorithm>
-    assert(is_permutation(sensors, unorderedSensors));
+  Timer timer;
 
-      // Sort the already sorted array using the STL sort.  This should be
-      // fast.
+  // Sort the Sensors using the STL sort algorithm.  It uses a variant
+  // of quicksort called introsort.
 
-    timer.start();
-    sort(sensors.begin(), sensors.end(), compareSensor);
-    report("STL sort if already sorted", timer.elapsed(), sensors);
-    assert(is_sorted(sensors.begin(), sensors.end(), compareSensor));
-    assert(is_permutation(sensors, unorderedSensors));
+  vector<Sensor> sensors(unorderedSensors);
+  timer.start();
+  sort(sensors.begin(), sensors.end(), compareSensor);
+  report("STL sort", timer.elapsed(), sensors);
+  assert(is_sorted(sensors.begin(), sensors.end(),
+                   compareSensor)); // from <algorithm>
+  assert(is_permutation(sensors, unorderedSensors));
 
-    if (TEST_INSERTION_SORT)
-    {
-          // Sort the original unsorted array using insertion sort.  This
-          // should be really slow.  If you have to wait more than a minute,
-          // try rerunning the program with a smaller number of Sensors.
+  // Sort the already sorted array using the STL sort.  This should be
+  // fast.
 
-        sensors = unorderedSensors;
-        timer.start();
-        insertion_sort(sensors, compareSensor);
-        double elapsed = timer.elapsed();
-        if (is_sorted(sensors.begin(), sensors.end(), compareSensor)  &&
-                is_permutation(sensors, unorderedSensors))
-        {
-            report("insertion sort if not already sorted", elapsed, sensors);
+  timer.start();
+  sort(sensors.begin(), sensors.end(), compareSensor);
+  report("STL sort if already sorted", timer.elapsed(), sensors);
+  assert(is_sorted(sensors.begin(), sensors.end(), compareSensor));
+  assert(is_permutation(sensors, unorderedSensors));
 
-              // Sort the already sorted array using insertion sort.  This
-              // should be fast.
+  if (TEST_INSERTION_SORT) {
+    // Sort the original unsorted array using insertion sort.  This
+    // should be really slow.  If you have to wait more than a minute,
+    // try rerunning the program with a smaller number of Sensors.
 
-            timer.start();
-            insertion_sort(sensors, compareSensor);
-            report("insertion sort if already sorted", timer.elapsed(), sensors);
-            assert(is_sorted(sensors.begin(), sensors.end(), compareSensor));
-            assert(is_permutation(sensors, unorderedSensors));
-        }
-        else
-            cout << "[Skipping tests of insertion_sort, since it's not correctly implemented.]" << endl;
-    }
-
-      // Since Sensors are expensive to copy, and since the STL's sort copies
-      // Sensors O(N log N) times, let's sort POINTERS to the Sensors, then
-      // make one final pass to rearrange the Sensors according to the
-      // reordered pointers.  We'll write some code; you write the rest
-      // below in sortUsingPtrs.
-
-      // Set sensors to the original unsorted sequence
     sensors = unorderedSensors;
-
-      // Start the timing
     timer.start();
+    insertion_sort(sensors, compareSensor);
+    double elapsed = timer.elapsed();
+    if (is_sorted(sensors.begin(), sensors.end(), compareSensor) &&
+        is_permutation(sensors, unorderedSensors)) {
+      report("insertion sort if not already sorted", elapsed, sensors);
 
-      // Sort using pointers
-    sortUsingPtrs(sensors, compareSensorPtr);
+      // Sort the already sorted array using insertion sort.  This
+      // should be fast.
 
-      // Report the timing and verify that the sort worked
-    report("STL sort of pointers", timer.elapsed(), sensors);
+      timer.start();
+      insertion_sort(sensors, compareSensor);
+      report("insertion sort if already sorted", timer.elapsed(), sensors);
+      assert(is_sorted(sensors.begin(), sensors.end(), compareSensor));
+      assert(is_permutation(sensors, unorderedSensors));
+    } else {
+      cout << "[Skipping tests of insertion_sort, since it's not correctly "
+              "implemented.]"
+           << endl;
+    }
+  }
 
-      // Until you correctly implement sortUsingPointers, this assertion will fail.
-    assert(is_sorted(sensors.begin(), sensors.end(), compareSensor));
-    assert(is_permutation(sensors, unorderedSensors));
+  // Since Sensors are expensive to copy, and since the STL's sort copies
+  // Sensors O(N log N) times, let's sort POINTERS to the Sensors, then
+  // make one final pass to rearrange the Sensors according to the
+  // reordered pointers.  We'll write some code; you write the rest
+  // below in sortUsingPtrs.
+
+  // Set sensors to the original unsorted sequence
+  sensors = unorderedSensors;
+
+  // Start the timing
+  timer.start();
+
+  // Sort using pointers
+  sortUsingPtrs(sensors, compareSensorPtr);
+
+  // Report the timing and verify that the sort worked
+  report("STL sort of pointers", timer.elapsed(), sensors);
+
+  // Until you correctly implement sortUsingPointers, this assertion will fail.
+  assert(is_sorted(sensors.begin(), sensors.end(), compareSensor));
+  assert(is_permutation(sensors, unorderedSensors));
 }
 
-void sortUsingPtrs(vector<Sensor>& sensors, bool comp(const Sensor*, const Sensor*))
-{
-      // Since Sensors are expensive to copy, and since the STL's sort copies
-      // Sensors O(N log N) times, let's sort POINTERS to the Sensors, then
-      // make one final pass to rearrange the Sensors according to the
-      // reordered pointers.  We'll write some code; you write the rest.
+void sortUsingPtrs(vector<Sensor> &sensors,
+                   bool /*comp*/ (const Sensor *, const Sensor *)) {
+  // Since Sensors are expensive to copy, and since the STL's sort copies
+  // Sensors O(N log N) times, let's sort POINTERS to the Sensors, then
+  // make one final pass to rearrange the Sensors according to the
+  // reordered pointers.  We'll write some code; you write the rest.
 
-      // Create an auxiliary copy of sensors to faciliate the later reordering.
-    vector<Sensor> auxSensors(sensors.begin(), sensors.end());
+  // Create an auxiliary copy of sensors to faciliate the later reordering.
+  vector<Sensor> auxSensors(sensors.begin(), sensors.end());
 
-      // TODO:  Create a vector of Sensor pointers, and set each pointer
-      //        to point to the corresponding Sensor in auxSensors.
+  // TODO:  Create a vector of Sensor pointers, and set each pointer
+  //        to point to the corresponding Sensor in auxSensors.
 
-      // TODO:  Sort the vector of pointers using the STL sort algorithm
-      //        with comp as the ordering relationship.
+  // TODO:  Sort the vector of pointers using the STL sort algorithm
+  //        with comp as the ordering relationship.
 
-      // TODO:  Using the now-sorted vector of pointers, replace each Sensor
-      //        in sensors with the Sensors from auxSensors in the correct
-      //        order.
+  // TODO:  Using the now-sorted vector of pointers, replace each Sensor
+  //        in sensors with the Sensors from auxSensors in the correct
+  //        order.
 
-      // auxSensors will be destroyed upon return from the function
+  // auxSensors will be destroyed upon return from the function
 }
