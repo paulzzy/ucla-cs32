095de800fe5123852eb3d05fc56d6fa2eb41a57a
Paul Zhang <paulz@ucla.edu>
2022-05-19 14:17:25 -0700

    Add Prof. Smallberg-provided code for Homework 4

---
+++ homework4/badlist.cpp
@@ -0,0 +1,76 @@
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+vector<int> destroyedOnes;
+
+class Restaurant
+{
+  public:
+    Restaurant(int s) : m_stars(s) {}
+    ~Restaurant() { destroyedOnes.push_back(m_stars); }
+    int stars() const { return m_stars; }
+  private:
+    int m_stars;
+};
+
+  // Remove the restaurants in li with 2 or fewer stars and destroy them.
+  // It is acceptable if the order of the remaining restaurants is not
+  // the same as in the original list.
+void removeBad(list<Restaurant*>& li)
+{
+}
+
+void test()
+{
+    int a[8] = { 4, 3, 2, 4, 1, 1, 5, 2 };
+    list<Restaurant*> x;
+    for (int k = 0; k < 8; k++)
+        x.push_back(new Restaurant(a[k]));
+    assert(x.size() == 8 && x.front()->stars() == 4 && x.back()->stars() == 2);
+    removeBad(x);
+    assert(x.size() == 4 && destroyedOnes.size() == 4);
+    vector<int> v;
+    for (list<Restaurant*>::iterator p = x.begin(); p != x.end(); p++)
+    {
+        Restaurant* rp = *p;
+        v.push_back(rp->stars());
+    }
+      // Aside:  Since C++11, the above loop could be
+      //     for (auto p = x.begin(); p != x.end(); p++)
+      //     {
+      //         Restaurant* rp = *p;
+      //         v.push_back(rp->stars());
+      //     }
+      // or    
+      //     for (auto p = x.begin(); p != x.end(); p++)
+      //     {
+      //         auto rp = *p;
+      //         v.push_back(rp->stars());
+      //     }
+      // or    
+      //     for (Restaurant* rp : x)
+      //         v.push_back(rp->stars());
+      // or    
+      //     for (auto rp : x)
+      //         v.push_back(rp->stars());
+    sort(v.begin(), v.end());
+    int expect[4] = { 3, 4, 4, 5 };
+    for (int k = 0; k < 4; k++)
+        assert(v[k] == expect[k]);
+    sort(destroyedOnes.begin(), destroyedOnes.end());
+    int expectGone[4] = { 1, 1, 2, 2 };
+    for (int k = 0; k < 4; k++)
+        assert(destroyedOnes[k] == expectGone[k]);
+    for (list<Restaurant*>::iterator p = x.begin(); p != x.end(); p++)
+        delete *p;
+}
+
+int main()
+{
+    test();
+    cout << "Passed" << endl;
+}
---
+++ homework4/evenlist.cpp
@@ -0,0 +1,33 @@
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+  // Remove the even integers from li.
+  // It is acceptable if the order of the remaining even integers is not
+  // the same as in the original list.
+void removeEven(list<int>& li)
+{
+}
+
+void test()
+{
+    int a[8] = { 1, 7, 6, 5, 8, 4, 3, 2 };
+    list<int> x(a, a+8);  // construct x from the array
+    assert(x.size() == 8 && x.front() == 1 && x.back() == 2);
+    removeEven(x);
+    assert(x.size() == 4);
+    vector<int> v(x.begin(), x.end());  // construct v from x
+    sort(v.begin(), v.end());
+    int expect[4] = { 1, 3, 5, 7 };
+    for (int k = 0; k < 4; k++)
+        assert(v[k] == expect[k]);
+}
+
+int main()
+{
+    test();
+    cout << "Passed" << endl;
+}
