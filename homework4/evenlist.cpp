590353861ff142e2518948bccf41ceea38380778
Paul Zhang <paulz@ucla.edu>
2022-05-19 16:03:09 -0700

    Complete problem 1a

--- homework4/evenlist.cpp
+++ homework4/evenlist.cpp
@@ -13,7 +13,17 @@ using namespace std;
 // Remove the even integers from li.
 // It is acceptable if the order of the remaining even integers is not
 // the same as in the original list.
-void removeEven(list<int> &li) {}
+void removeEven(list<int> &li) {
+  auto it = li.begin();
+
+  while (it != li.end()) {
+    if (*it % 2 == 0) {
+      it = li.erase(it);
+    } else {
+      it++;
+    }
+  }
+}
 
 #ifdef DO_NOT_SUBMIT_FOR_HOMEWORK_SIX
 
