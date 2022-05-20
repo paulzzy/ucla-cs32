8b684ffeb604403958df6c90cf11b2ac891a8257
Paul Zhang <paulz@ucla.edu>
2022-05-20 12:38:41 -0700

    Modify `Sequence` to be a template class

--- homework4/Sequence.h
+++ homework4/Sequence.h
@@ -3,15 +3,15 @@
 #ifndef SEQUENCE_INCLUDED
 #define SEQUENCE_INCLUDED
 
-using ItemType = unsigned long;
+using T = unsigned long;
 
-class Sequence {
-public:
+// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
+template <typename T> class Sequence {
   Sequence();         // Create an empty sequence (i.e., one whose size() is 0).
   bool empty() const; // Return true if the sequence is empty, otherwise false.
   int size() const;   // Return the number of items in the sequence.
 
-  int insert(int pos, const ItemType &value);
+  int insert(int pos, const T &value);
   // Insert value into the sequence so that it becomes the item at
   // position pos.  The original item at position pos and those that
   // follow it end up at positions one greater than they were at before.
@@ -21,7 +21,7 @@ public:
   // leave the sequence unchanged and return -1.  Notice that
   // if pos is equal to size(), the value is inserted at the end.
 
-  int insert(const ItemType &value);
+  int insert(const T &value);
   // Let p be the smallest integer such that value <= the item at
   // position p in the sequence; if no such item exists (i.e.,
   // value > all items in the sequence), let p be size().  Insert
@@ -37,21 +37,21 @@ public:
   // positions one lower than they were at before), and return true.
   // Otherwise, leave the sequence unchanged and return false.
 
-  int remove(const ItemType &value);
+  int remove(const T &value);
   // Erase all items from the sequence that == value.  Return the
   // number of items removed (which will be 0 if no item == value).
 
-  bool get(int pos, ItemType &value) const;
+  bool get(int pos, T &value) const;
   // If 0 <= pos < size(), copy into value the item at position pos
   // of the sequence and return true.  Otherwise, leave value unchanged
   // and return false.
 
-  bool set(int pos, const ItemType &value);
+  bool set(int pos, const T &value);
   // If 0 <= pos < size(), replace the item at position pos of the
   // sequence with value and return true.  Otherwise, leave the sequence
   // unchanged and return false.
 
-  int find(const ItemType &value) const;
+  int find(const T &value) const;
   // Let p be the smallest integer such that value == the item at
   // position p in the sequence; if no such item exists, let p be -1.
   // Return p.
@@ -75,18 +75,18 @@ private:
   //       m_head->prev points to the node at position m_size-1.
 
   struct Node {
-    ItemType m_value;
+    T m_value;
     Node *m_next;
     Node *m_prev;
   };
 
-  Node *m_head;
-  int m_size;
+  Node *m_head{};
+  int m_size{};
 
   void createEmpty();
   // Create an empty list.  (Should be called only by constructors.)
 
-  void insertBefore(Node *p, const ItemType &value);
+  void insertBefore(Node *p, const T &value);
   // Insert value in a new Node before Node p, incrementing m_size.
 
   Node *doErase(Node *p);
@@ -100,18 +100,21 @@ private:
 
 // Declarations of non-member functions
 
-int subsequence(const Sequence &seq1, const Sequence &seq2);
+int subsequence(const Sequence<T> &seq1, const Sequence<T> &seq2);
 // If seq2 is a contiguous subsequence of seq1, return the position in
 // seq1 where that subsequence starts (the earliest such position if more
 // than one).  If not, or if seq2 is empty, return -1.
 
-void interleave(const Sequence &seq1, const Sequence &seq2, Sequence &result);
+void interleave(const Sequence<T> &seq1, const Sequence<T> &seq2,
+                Sequence<T> &result);
 // Set result to a Sequence that interleaves seq1 and seq2.
 
 // Inline implementations
 
-inline int Sequence::size() const { return m_size; }
+template <typename T> inline int Sequence<T>::size() const { return m_size; }
 
-inline bool Sequence::empty() const { return size() == 0; }
+template <typename T> inline bool Sequence<T>::empty() const {
+  return size() == 0;
+}
 
 #endif // SEQUENCE_INCLUDED
